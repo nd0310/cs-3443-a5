3a12891b96e61d01dfc23c21b80b68b5478d5370
nd0310 <ndedu10@gmail.com>
2025-11-25 12:02:32 -0600

    implement CountDownTimer class and add Runnable interface imlementations

--- src/application/model/CountDownTimer.java
+++ src/application/model/CountDownTimer.java
@@ -2,14 +2,32 @@ package application.model;
 
 import application.view.MainView;
 
-public class CountDownTimer {
-
+/**
+ * @author Nathan Davies
+ * Class implementing {@code Runnable} interface for keeping track of game time
+ */
+public class CountDownTimer implements Runnable {
+  
   private WhackAMole game;
   private MainView mainView;
   private int durationSeconds;
-
-  public synchronized void run() {
-    throw new RuntimeException("Not Implemented");
+  
+  /**
+   * @author Nathan Davies
+   * Counts down {@code durationSeconds} updating the time remaining label
+   * and calling {@code WhackAMole.endGame()} after the timer reaches zero
+   * */
+  @Override
+  public synchronized void run()  {
+    while (durationSeconds > 0) {
+      try {
+        Thread.sleep(1000);
+      } catch (Exception e) {
+        throw new RuntimeException("Unreachable");
+      }
+      durationSeconds -= 1;
+      mainView.displayRemainingTime();
+    }
+    game.endGame();
   }
-
-}
+} 
--- src/application/model/Mole.java
+++ src/application/model/Mole.java
@@ -5,14 +5,15 @@ import java.util.Random;
 import application.view.MainView;
 import javafx.scene.image.Image;
 
-public class Mole {
+public class Mole implements Runnable {
 
   private WhackAMole game;
   private MainView mainView;
   private Image moleImage;
   private int index;
   private Random rand;
-
+  
+  @Override
   public synchronized void run() {
     throw new RuntimeException("Not Implemented");
   }
